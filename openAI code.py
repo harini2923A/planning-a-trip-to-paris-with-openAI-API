4e4debe3d93979648929dccb5af5f8d1f798c770
harini2923A <harini2923@gmail.com>
2025-11-09 18:07:40 -0500

    Fix indentation and improve user message handling

--- openAI code.py
+++ openAI code.py
@@ -17,16 +17,20 @@ user_msgs = [ "How far away is the Louvre from the Eiffel Tower (in miles) if yo
     "Where is the Arc de Triomphe?",
     "What are the must-see artworks at the Louvre Museum?"
             ]
+# Loop over the user questions
 for q in user_msgs:
-    print("User: ", q)    
+    print("User: ", q)   
+     # Create a dictionary for the user message from q and append to conversation
     user_dict = {"role": "user", "content": q}    
     conversation.append(user_dict)  
+    # Create the API request
     response = client.chat.completions.create(
       model="gpt-4o-mini",
       messages=conversation,  
       max_completion_tokens=100,
       temperature=0.0
 )  
+# Append the assistant's converstion to messages
     assistant_dict = {"role": "assistant", "content": response.choices[0].message.content}
     conversation.append(assistant_dict)
     print("Assistant: ", response.choices[0].message.content, "\n")
