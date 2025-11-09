13658b439399ca4e4253e53914fadc0a6d45e17e
harini2923A <harini2923@gmail.com>
2025-11-09 18:03:54 -0500

    Code

---
+++ code.py
@@ -0,0 +1,32 @@
+# Start your code here!
+import os
+from openai import OpenAI
+
+# Define the model to use
+model = "gpt-4o-mini"
+
+# Define the client
+client = OpenAI()
+
+# Start coding here
+# Add as many cells as you like
+conversation=[
+    {"role": "system","content": "Parisian expert, delivering valuable insights into the city's iconic landmarks and hidden treasures.user friendly"}
+]         
+user_msgs = [ "How far away is the Louvre from the Eiffel Tower (in miles) if you are driving?",
+    "Where is the Arc de Triomphe?",
+    "What are the must-see artworks at the Louvre Museum?"
+            ]
+for q in user_msgs:
+    print("User: ", q)    
+    user_dict = {"role": "user", "content": q}    
+    conversation.append(user_dict)  
+    response = client.chat.completions.create(
+      model="gpt-4o-mini",
+      messages=conversation,  
+      max_completion_tokens=100,
+      temperature=0.0
+)  
+    assistant_dict = {"role": "assistant", "content": response.choices[0].message.content}
+    conversation.append(assistant_dict)
+    print("Assistant: ", response.choices[0].message.content, "\n")
